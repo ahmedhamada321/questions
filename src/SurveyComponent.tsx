914ffa5bacbb9f53cf22e0237ffcd8a473e537ec
Ahmed Hamada <ahmedhamada5432112345@gmail.com>
2025-03-02 14:30:13 +0200

    commit

--- src/SurveyComponent.tsx
+++ src/SurveyComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
 import { Model } from "survey-core";
 import { Survey } from "survey-react-ui";
 import "survey-core/defaultV2.min.css";
